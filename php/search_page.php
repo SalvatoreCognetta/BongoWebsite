b5986ccc3230bc9283828ccd103667886e6f058f
Salvatore Cognetta <salvatore.cognetta@gmail.com>
2017-11-30 22:48:38 +0100

    Modificato il css inserendo immagini e cambiano stile al testo. Iniziato la pagina php.

---
+++ php/search_page.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+	<title>Bongo</title>    
+	<link href="../css/style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+	<header>
+		<nav>
+			<div class="nav-container">
+				<h1>Bongo</h1>
+				<ul>
+					<li class="active"><a href="./home.html">Home</a></li>
+					<li><a href="./login.html">Accedi</a></li>
+					<li><a href="./signin.html">Registrati</a></li>
+					<li><a href="./help.html">Aiuto</a></li>
+					<li><a href="./about.html">Chi siamo</a></li>
+				</ul>    
+			</div>
+		</nav>
+	</header>
+<?php
+echo "Mssyscsadsadasdsaript!";
+?>
+
+</body>
+
+</html>
