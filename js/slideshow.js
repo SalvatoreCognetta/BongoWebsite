f62c2e4b8643fbc576fae585ad32e6e26adfacbc
Salvatore Cognetta <salvatore.cognetta@gmail.com>
2017-12-05 12:46:55 +0100

    Modifiche alla search_pahe

---
+++ js/slideshow.js
@@ -0,0 +1,21 @@
+/*var images = [
+	'url("../img/party.jpeg")',
+	'url(\"../img/light.jpeg\"\)',
+	'url("../img/bar.jpeg")'
+];
+var num = 0;
+var x = document.getElementsByClassName("wrap-container");
+
+setInterval(function () {
+	next()
+}, 5000);
+
+function next() {
+	num++;
+	if (num >= images.length) {
+		num = 0;
+	}
+	x[0].style.background = images[num];
+	x[0].style.backgroundSize = "cover";
+	
+}*/
--- php/search_page.php
+++ php/search_page.php
@@ -13,18 +13,65 @@
 			<div class="nav-container">
 				<h1>Bongo</h1>
 				<ul>
-					<li class="active"><a href="./home.html">Home</a></li>
-					<li><a href="./login.html">Accedi</a></li>
-					<li><a href="./signin.html">Registrati</a></li>
-					<li><a href="./help.html">Aiuto</a></li>
-					<li><a href="./about.html">Chi siamo</a></li>
+					<li><a href="../html/home.html">Home</a></li>
+					<li><a href="../html/login.html">Accedi</a></li>
+					<li><a href="../html/signin.html">Registrati</a></li>
+					<li><a href="../html/help.html">Aiuto</a></li>
+					<li><a href="../html/about.html">Chi siamo</a></li>
 				</ul>    
 			</div>
 		</nav>
 	</header>
+
+	<div class="filter-container">
+		<aside>
+			<label class="test-container"> One
+				<input type="checkbox" checked="checked">
+				<span class="checkmark"></span>
+			</label>
+
+			<form>
+				<fieldset>
+					<legend>Tipo di eventi</legend>
+					<input type="checkbox" name="" id="">test<br/>
+					
+				</fieldset>
+			</form>
+			<h3>Tipo di eventi</h3>
+			<ul>
+			<li>test</li>
+			</ul>
+		</aside>
+		
+	</div>
+
 <?php
-echo "Mssyscsadsadasdsaript!";
-?>
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eventi_bongo";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM evento";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["idevento"]. " - Name: " . $row["nomeEvento"]. " - Città:" . $row["citta"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?> 
 
 </body>
 
